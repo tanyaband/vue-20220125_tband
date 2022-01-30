b5492a2266f92f263316436fa13d6fd46f658646
Tatiana Bandurina <tbanduri@gmail.com>
2022-01-30 15:56:31 +0300

    basic calculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,33 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const app = createApp({
+  data() {
+    return {
+      firstvalue: null,
+      secondvalue: null,
+      operation: null,
+    };
+  },
+
+  computed: {
+    result() {
+      if (!this.firstvalue || !this.secondvalue || !this.operation) {
+        return null;
+      }
+      switch (this.operation) {
+        case 'sum':
+          return this.firstvalue + this.secondvalue;
+        case 'subtract':
+          return this.firstvalue - this.secondvalue;
+        case 'multiply':
+          return this.firstvalue * this.secondvalue;
+        case 'divide':
+          return this.firstvalue / this.secondvalue;
+        default:
+          return null;
+      }
+    },
+  },
+});
+
+app.mount('#app');
