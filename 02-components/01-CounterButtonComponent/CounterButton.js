a777c27d63fab76b2818d0643c088bf56d5a67ce
Tatiana Bandurina <tbanduri@gmail.com>
2022-02-03 18:55:10 +0300

    02-components 01-CounterButtonComponent

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -4,9 +4,16 @@ export default defineComponent({
   name: 'CounterButton',
 
   // Компонент должен иметь входной параметр
+  props: {
+    count: { type: Number, default: 0 },
+  },
 
   // Шаблон лучше держать максимально простым, а логику выносить в методы
-
+  methods: {
+    onClick() {
+      this.$emit('update:count', this.count + 1);
+    },
+  },
   // Шаблон потребуется отредактировать
-  template: `<button type="button">1</button>`,
+  template: `<button type="button" @click="onClick">{{ count }}</button>`,
 });
