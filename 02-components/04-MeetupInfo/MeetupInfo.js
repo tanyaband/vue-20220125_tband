13900c184e67f7ef4f127b3aa9145c5175143fcb
Tatiana Bandurina <tbanduri@gmail.com>
2022-02-04 11:29:55 +0300

    02-components 04-MeetupInfo

--- 02-components/04-MeetupInfo/MeetupInfo.js
+++ 02-components/04-MeetupInfo/MeetupInfo.js
@@ -3,19 +3,39 @@ import { defineComponent } from './vendor/vue.esm-browser.js';
 export default defineComponent({
   name: 'MeetupInfo',
 
+  props: {
+    organizer: { type: String, require: true },
+    place: { type: String, require: true },
+    date: { type: Number, require: true },
+  },
+
+  computed: {
+    dateForUser() {
+      return new Date(this.date).toLocaleDateString(navigator.language, {
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric',
+      });
+    },
+
+    dateISO() {
+      return new Date(this.date).toISOString().substring(0, 10);
+    },
+  },
+
   template: `
     <ul class="meetup-info">
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-user.svg" />
-        Организатор
+        {{ organizer}}
       </li>
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-map.svg" />
-        Место
+        {{ place }}
       </li>
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-cal-lg.svg" />
-        <time datetime="2020-01-01">1 янв. 2020</time>
+        <time :datetime="dateISO">{{ dateForUser }}</time>
       </li>
     </ul>`,
 });
