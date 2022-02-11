b234f3502bdd013a13722ab42f422bcbefaf0a32
javascriptru <67475495+jsru-1@users.noreply.github.com>
2022-02-11 15:26:40 +0300

    Added task UiFormGroup1

---
+++ 04-vue-cli/01-UiFormGroup1/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <ui-container class="sample">
+    <ui-form-group>
+      <div class="input-group">
+        <input class="form-control" placeholder="No label" />
+      </div>
+    </ui-form-group>
+
+    <hr />
+
+    <ui-form-group label="Label Text">
+      <div class="input-group">
+        <input class="form-control" placeholder="With label" />
+      </div>
+    </ui-form-group>
+
+    <hr />
+
+    <div>
+      <ui-form-group inline>
+        <div class="input-group">
+          <input class="form-control" placeholder="Inline" />
+        </div>
+      </ui-form-group>
+
+      <ui-form-group inline>
+        <div class="input-group">
+          <input class="form-control" placeholder="Inline" />
+        </div>
+      </ui-form-group>
+    </div>
+  </ui-container>
+</template>
+
+<script>
+import UiFormGroup from './components/UiFormGroup';
+import UiContainer from './components/UiContainer';
+
+export default {
+  name: 'App',
+
+  components: { UiContainer, UiFormGroup },
+};
+</script>
+
+<style scoped>
+hr {
+  margin: 1rem auto;
+}
+</style>
