0174c825a42c2293cc720ea0eda8a2c63c7e61c7
javascriptru <67475495+jsru-1@users.noreply.github.com>
2022-02-16 11:22:24 +0300

    Added task UiDropdown1

---
+++ 04-vue-cli/03-UiDropdown1/App.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="sample container">
+    <p>selectedType = {{ selectedType }}</p>
+    <p>
+      <button @click="selectedType = 'registration'">Set registration</button>
+    </p>
+    <p>
+      <ui-dropdown v-model="selectedType" :options="agendaItemTypes" title="Событие" />
+    </p>
+    <p>selectedLang = {{ selectedLang }}</p>
+    <p>
+      <ui-dropdown v-model="selectedLang" :options="langOptions" title="Язык" />
+    </p>
+  </div>
+</template>
+
+<script>
+import UiDropdown from './components/UiDropdown';
+
+const agendaIconMap = {
+  registration: 'key',
+  opening: 'cal-sm',
+  talk: 'tv',
+  break: 'clock',
+  coffee: 'coffee',
+  closing: 'key',
+  afterparty: 'cal-sm',
+  other: 'cal-sm',
+};
+
+const agendaItemTypes = [
+  {
+    value: 'registration',
+    text: 'Регистрация',
+  },
+  {
+    value: 'opening',
+    text: 'Открытие',
+  },
+  {
+    value: 'break',
+    text: 'Перерыв',
+  },
+  {
+    value: 'coffee',
+    text: 'Coffee Break',
+  },
+  {
+    value: 'closing',
+    text: 'Закрытие',
+  },
+  {
+    value: 'afterparty',
+    text: 'Afterparty',
+  },
+  {
+    value: 'talk',
+    text: 'Доклад',
+  },
+  {
+    value: 'other',
+    text: 'Другое',
+  },
+].map((item) => ({
+  ...item,
+  icon: agendaIconMap[item.value],
+}));
+
+export default {
+  name: 'App',
+  components: { UiDropdown },
+  data() {
+    return {
+      agendaItemTypes,
+
+      langOptions: [
+        { value: 'EN', text: 'EN' },
+        { value: 'RU', text: 'RU' },
+      ],
+
+      selectedLang: 'EN',
+      selectedType: undefined,
+    };
+  },
+};
+</script>
+
+<style></style>
---
+++ 04-vue-cli/03-UiDropdown1/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue';
+import App from './App';
+import '@/assets/styles/app.css';
+import '@/assets/styles/taskbook.css';
+import '@/assets/styles/_container.css';
+
+createApp(App).mount('#app');
