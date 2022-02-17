4574eb63fa24dfb4801aaf45236b0c2099535725
javascriptru <67475495+jsru-1@users.noreply.github.com>
2022-02-17 10:04:05 +0300

    Added task AuthPages - основы VueRouter

---
+++ 05-vue-router/01-AuthPages/components/MeetupsHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="header">
+    <meetups-logo />
+    <meetups-nav />
+  </header>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import MeetupsLogo from './MeetupsLogo';
+import MeetupsNav from './MeetupsNav';
+
+export default defineComponent({
+  name: 'MeetupsHeader',
+  components: { MeetupsNav, MeetupsLogo },
+});
+</script>
+
+<style scoped>
+.header {
+  padding: 44px 0 32px;
+  background-color: var(--white);
+}
+</style>
---
+++ 05-vue-router/01-AuthPages/views/PageRegister.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="page-auth">
+    <ui-container>
+      <h2 class="title page-auth__title">Регистрация</h2>
+      <form class="form" @submit.prevent="handleSubmit">
+        <ui-form-group label="Email">
+          <div class="input-group">
+            <input name="email" type="email" class="form-control" />
+          </div>
+        </ui-form-group>
+        <ui-form-group label="Имя">
+          <div class="input-group">
+            <input name="fullname" type="text" class="form-control" />
+          </div>
+        </ui-form-group>
+        <ui-form-group label="Пароль">
+          <div class="input-group">
+            <input name="password" type="password" class="form-control" />
+          </div>
+        </ui-form-group>
+        <ui-form-group label="Повтор пароля">
+          <div class="input-group">
+            <input type="password" class="form-control" />
+          </div>
+        </ui-form-group>
+        <ui-form-group>
+          <label class="checkbox">
+            <input type="checkbox" class="checkbox__input" />
+            <span class="checkbox__box"></span>
+            Я согласен с условиями
+          </label>
+        </ui-form-group>
+        <div class="form__buttons">
+          <button type="submit" class="button button_primary">Зарегистрироваться</button>
+        </div>
+        <div class="form__append">Уже есть аккаунт? <a href="/login" class="link">Войдите</a></div>
+      </form>
+    </ui-container>
+  </div>
+</template>
+
+<script>
+import UiFormGroup from '../components/UiFormGroup';
+import UiContainer from '../components/UiContainer';
+
+export default {
+  name: 'PageRegister',
+
+  components: {
+    UiFormGroup,
+    UiContainer,
+  },
+
+  methods: {
+    handleSubmit() {
+      // Требуется обработать сабмит формы
+    },
+  },
+};
+</script>
+
+<style></style>
